fc428d42987538c36a2ea7eecbe4dd53a3f3421b
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-03-01 14:29:38 +0100

    Added Post Model, updated views and admin panel, migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now) # if it was, auto_now_add=True it wouldn't be able to be updated 
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # if a user is deleted, his posts will be deleted as well
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Lukasz Pasiak',
-        'title': 'Post #1',
-        'content': 'First post content',
-        'date_posted': 'February 27, 2025'
-    },
-    {
-        'author': 'Lukasz Psiapsiak',
-        'title': 'Post #2',
-        'content': 'Second post content',
-        'date_posted': 'February 28, 2025'
-    },
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
 
     return render(request, 'blog/home.html', context)
