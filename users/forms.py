72e86d0446300657a85aa9a5bec93a37082d84a1
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-03-02 18:28:56 +0100

    Add Login, Logout system, Profile view and template

--- users/forms.py
+++ users/forms.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField(
+        help_text='Required. Enter a valid email address that will be used for account verification.'
+    )
 
     class Meta:
         model = User
