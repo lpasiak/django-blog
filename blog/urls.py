2baf6e26cb9ab43b254a1faffc3182c592e272d1
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-03-09 03:31:07 +0100

    Filtering by a post author

--- blog/urls.py
+++ blog/urls.py
@@ -4,12 +4,14 @@ from .views import (
     PostDetailView,
     PostCreateView,
     PostUpdateView,
-    PostDeleteView
+    PostDeleteView,
+    UserPostListView
 )
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'), # The template is expected to be a name of the Model_form
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
